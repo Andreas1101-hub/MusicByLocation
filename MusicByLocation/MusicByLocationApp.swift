773922f5a514ee98726cfb0c66d818345897562c
Andreas Kwong <andreaskwong@gmail.com>
2024-03-19 18:14:57 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Andreas Kwong on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
